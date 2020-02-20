f60c0aa47ace776af06f8a7614f07158d8f89a06
Zac Isaacson <isaacson.zm@gmail.com>
2020-02-20 11:59:51 -0700

    Complete POST request

--- src/index.js
+++ src/index.js
@@ -9,4 +9,26 @@ $(document).ready(() => {
     document.getElementById('top-word-title').innerHTML = Object.keys(topWordTitle.word)[0]
     document.getElementById('top-word-count').innerHTML = Object.values(topWordTitle.word)[0]
   })
+  document.getElementById('submit').addEventListener('click', createText)
 })
+
+function createText() {
+  event.preventDefault();
+
+  let text = document.getElementById('text').value;
+
+  fetch('https://wordwatch-api.herokuapp.com/api/v1/words', {
+    method: 'POST',
+    headers :
+      {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      word: {
+        value: text
+      }
+    })
+  })
+  .then(res => res.json())
+  .then(resJson => {
+  console.log(resJson)
+  })
+}
